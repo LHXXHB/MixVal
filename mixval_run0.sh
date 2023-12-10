07b2981ef999d9aa9117c0953cb223dceca8b81d
LHXXHB <dphnju@163.com>
2023-12-10 14:57:07 +0800

    add scripts

---
+++ mixval_run0.sh
@@ -0,0 +1,33 @@
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.02 --run 0  
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.05 --run 0  
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.1 --run 0  
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.2 --run 0  
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.5 --run 0  
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 1.0 --run 0  
+python val_mixval_intra.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 2.0 --run 0  
+
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.02 --run 0  
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.05 --run 0  
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.1 --run 0  
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.2 --run 0  
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 0.5 --run 0  
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 1.0 --run 0  
+python val_mixval_inter.py --dset office-home --s 0 --t 1 --gpu_id 1 --da uda --method BNM --hyperparam 2.0 --run 0  
+
+'''
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.05 --run 0 
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.1 --run 0 
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.2 --run 0 
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.5 --run 0 
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 1.0 --run 0 
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 2.0 --run 0 
+python val_mixval_intra.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 5.0 --run 0 
+
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.05 --run 0 
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.1 --run 0 
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.2 --run 0 
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 0.5 --run 0 
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 1.0 --run 0 
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 2.0 --run 0 
+python val_mixval_inter.py --dset office-home --s 0 --t 2 --gpu_id 1 --da pda --method PADA --hyperparam 5.0 --run 0 
+'''
